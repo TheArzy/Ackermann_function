e7351e29614e0dd1827d238e612ceb1db658a0d6
TheArzy <84496829+TheArzy@users.noreply.github.com>
2021-05-20 09:37:29 +0200

    Add files via upload

---
+++ 1_5_5_Ackermann_function/1_5_5_Ackermann_function/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace _1_5_5_Ackermann_function
+{
+    class Program
+    {
+        /// <summary>
+        /// Ввод числа и проверка соответствия условиям
+        /// </summary>
+        /// <param name="paramMax">Максимальное значение для вводимого числа</param>
+        /// <param name="paramMin">Минимальное значение для вводимого числа</param>
+        /// <returns>Проверенное введенное число</returns>
+        static long Input(long paramMax, long paramMin)
+        {
+            long input;
+            while (true)
+            {
+                if (long.TryParse(ReadLine(), out input) && input <= paramMax && input >= paramMin) break;
+                else Write($"Число должно быть целым от {paramMin} до {paramMax}, попробуйте еще раз: ");
+            }
+            return input;
+        }
+
+        /// <summary>
+        /// Функция Аккермана
+        /// </summary>
+        /// <param name="mom">Параметр мом</param>
+        /// <param name="nom">Параметр ном</param>
+        /// <returns>Значение функции при заданных параметрах</returns>
+        static long AckerFun(long mom, long nom)
+        {
+            if (mom == 0) return nom + 1;
+            else if (mom > 0 & nom == 0) return AckerFun(mom - 1, 1);
+            else if (mom > 0 & nom > 0) return AckerFun(mom - 1, AckerFun(mom, nom - 1));
+            return 0;
+        }
+
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                WriteLine("--------------------------------\n");
+                WriteLine("Введите первый и второй параметры функции:");
+                WriteLine(AckerFun(Input(long.MaxValue, long.MinValue), Input(long.MaxValue, long.MinValue)));
+                WriteLine("\n--------------------------------\n");
+
+                #region Повтор или выход
+
+                WriteLine("Запустить заново? 1 - Повтор | 0 - Выход");
+                Write("Выбор: ");
+                if (Input(1, 0) == 0) break; // Завершение программы
+                WriteLine();
+
+                #endregion
+            }
+        }
+    }
+}
---
+++ 1_5_5_Ackermann_function/1_5_5_Ackermann_function/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные с этой сборкой.
+[assembly: AssemblyTitle("1_5_5_Ackermann_function")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("1_5_5_Ackermann_function")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// из модели COM задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
+[assembly: Guid("e01fbdea-d62c-4b90-8e6b-9fa3cc64e881")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Номер редакции
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
